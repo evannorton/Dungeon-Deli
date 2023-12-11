8accb2bdf83b126d326b83ebf162ebac03b8b686
Evan Norton <evan@retro-mmo.com>
2023-12-10 23:47:38 -0500

    Edit instructions

--- src/functions/createInstructionsHUD.ts
+++ src/functions/createInstructionsHUD.ts
@@ -8,7 +8,7 @@ import { state } from "../state";
 export const createInstructionsHUD = (): void => {
   const centerX: number = Math.floor(480 / 2);
   const width: number = 302 + 12;
-  const height: number = 67;
+  const height: number = 99;
   const x: number = centerX - Math.floor(width / 2);
   const y: number = 24;
   const condition = (): boolean => state.values.instructionsOpen;
@@ -38,40 +38,60 @@ export const createInstructionsHUD = (): void => {
     coordinates: {
       condition,
       x: x + 6,
-      y: y + 18,
+      y: y + 22,
     },
     horizontalAlignment: "left",
-    text: "- Arrow keys/WASD/D-pad to move.",
+    text: "Every step is an action.",
   });
   createLabel({
     color: "#ffffff",
     coordinates: {
       condition,
       x: x + 6,
-      y: y + 30,
+      y: y + 34,
     },
     horizontalAlignment: "left",
-    text: "- Powers trigger as you step.",
+    text: "- Arrow keys/WASD/D-Pad to move",
   });
   createLabel({
     color: "#ffffff",
     coordinates: {
       condition,
       x: x + 6,
-      y: y + 42,
+      y: y + 46,
     },
     horizontalAlignment: "left",
-    text: "- Click/space/A button to wait.",
+    text: "- Click/space/A button to wait (or close menus).",
   });
   createLabel({
     color: "#ffffff",
     coordinates: {
       condition,
       x: x + 6,
-      y: y + 54,
+      y: y + 62,
     },
     horizontalAlignment: "left",
-    text: "- Click/space/A button to close menus.",
+    text: "Different terrains influence...",
+  });
+  createLabel({
+    color: "#ffffff",
+    coordinates: {
+      condition,
+      x: x + 6,
+      y: y + 74,
+    },
+    horizontalAlignment: "left",
+    text: "- Powers: Your attacks.",
+  });
+  createLabel({
+    color: "#ffffff",
+    coordinates: {
+      condition,
+      x: x + 6,
+      y: y + 86,
+    },
+    horizontalAlignment: "left",
+    text: "- Modes: Effects on the field",
   });
   createInputPressHandler({
     condition,
