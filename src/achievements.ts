ebba9e3d95bdaf1a6ac63da2101ae1003e713665
Evan Norton <enorton@citystateentertainment.com>
2024-01-23 12:22:04 -0500

    Add newgrounds medal IDs

--- src/achievements.ts
+++ src/achievements.ts
@@ -5,34 +5,40 @@ export const lifestealModeAchievementID: string = createAchievement({
   id: "lifesteal-mode",
   imagePath: "achievements/lifesteal-mode",
   name: "Full Restore",
+  newgroundsMedalID: 76890,
 });
 export const reverseModeAchievementID: string = createAchievement({
   description: "Walk into a wall during Reverse Mode.",
   id: "reverse-mode",
   imagePath: "achievements/reverse-mode",
   name: "Confusion",
+  newgroundsMedalID: 76893,
 });
 export const slipperyModeAchievementID: string = createAchievement({
   description: "Slide into a staircase during Slippery Mode.",
   id: "slippery-mode",
   imagePath: "achievements/slippery-mode",
   name: "Slippery Stairs",
+  newgroundsMedalID: 76892,
 });
 export const knockbackModeAchievementID: string = createAchievement({
   description: "Push a minecart mouse off of its tracks during Knockback Mode.",
   id: "knockback-mode",
   imagePath: "achievements/knockback-mode",
   name: "Derailed",
+  newgroundsMedalID: 76891,
 });
 export const clearGameAchievementID: string = createAchievement({
   description: "Clear the dungeon and retrieve your sandwich.",
   id: "clear-game",
   imagePath: "achievements/clear-game",
   name: "Picnic Time",
+  newgroundsMedalID: 76888,
 });
 export const parTimesAchievementID: string = createAchievement({
   description: "Achieve the goal steps for each level.",
   id: "par-times",
   imagePath: "achievements/par-times",
   name: "Dungeoneer",
+  newgroundsMedalID: 76889,
 });
